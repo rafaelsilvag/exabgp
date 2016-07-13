c0c7baa1afe249a70fa797763df5800bd8f4c808
Job Snijders <job@instituut.net>
2016-07-13 10:13:26 +0200

    Add Large BGP Communities attribute value

--- lib/exabgp/bgp/message/update/attribute/attribute.py
+++ lib/exabgp/bgp/message/update/attribute/attribute.py
@@ -75,6 +75,9 @@ class Attribute (object):
 		TUNNEL_ENCAP       = 0x17  # 23
 		AIGP               = 0x1A  # 26
 
+        # draft-heitz-idr-large-community-00
+        LARGE_COMMUNITY    = 0x29  # 41
+
 		INTERNAL_NAME      = 0xFFFC
 		INTERNAL_WITHDRAW  = 0xFFFD
 		INTERNAL_WATCHDOG  = 0xFFFE
@@ -99,6 +102,7 @@ class Attribute (object):
 			PMSI_TUNNEL:        'pmsi-tunnel',
 			TUNNEL_ENCAP:       'tunnel-encaps',
 			AIGP:               'aigp',
+            LARGE_COMMUNITY:    'large-community',
 			0xfffc:             'internal-name',
 			0xfffd:             'internal-withdraw',
 			0xfffe:             'internal-watchdog',
